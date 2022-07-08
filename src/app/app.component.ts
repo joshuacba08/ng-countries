fb586048dd6ecb500119887a1c5493df3c40ab2b
joroya.hms <47667368+joshuacba08@users.noreply.github.com>
2022-07-08 20:22:57 -0300

    componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,28 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'ng-countries';
+  
+  countries = [
+    {
+      id:1,
+      name:"uruguay",
+      description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      flag:"https://media.istockphoto.com/photos/uruguay-national-flag-waving-on-pole-against-sunny-blue-sky-high-picture-id1129174937?k=20&m=1129174937&s=612x612&w=0&h=yohmeYykGaVK3epwoVmfSY8FKnpG6a8P8TKfCSBrDzY="
+    },
+    {
+      id:2,
+      name:"argentina",
+      description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      flag:"https://media.istockphoto.com/photos/argentinian-flag-picture-id178392703?k=20&m=178392703&s=612x612&w=0&h=wA-Dylj5lssiaZxLv9l20tWKNxHEKkZSmaN56iRlUP8="
+    },
+    {
+      id:3,
+      name:"mexico",
+      description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      flag:"https://images.pexels.com/photos/12668023/pexels-photo-12668023.jpeg?auto=compress&cs=tinysrgb&w=1600"
+    },
+  ]
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import {MatGridListModule} from '@angular/material/grid-list';
+import { MatSliderModule } from '@angular/material/slider';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CountryCardComponent } from './components/country-card/country-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CountryCardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatIconModule,
+    MatGridListModule,
+    MatSliderModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
